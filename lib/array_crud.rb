6c6741962c0107e177a56bfaadfe5b1c60deb44a
Erick Villeda <eeverickeev@gmail.com>
2020-09-01 22:10:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Muay Thai", "Boxing", "Wrestling", "MMA"]
+  Fighting_styles = ["Muay Thai", "Boxing", "Wrestling", "MMA"]
 end
 
 def add_element_to_end_of_array(array, element)
